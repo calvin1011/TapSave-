cfc94af8f5426610a4e1a2cfce7178822b5b1a9b
calvin1011 <calvinssendawula@gmail.com>
2026-01-03 21:48:28 -0600

    gradle build dependencies error fixed and tests passing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("com.google.android.material:material:1.13.0")
 
     // Hilt
     implementation(libs.hilt.android)
